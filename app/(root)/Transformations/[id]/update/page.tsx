2ad94c84e90431c9391e81eeaa791e27b6b2f57a
Arshad <arshadmulla75@gmail.com>
2024-02-29 11:58:20 +0530

    inital commit

---
+++ app/(root)/Transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateTransformationPage = () => {
+  return (
+    <div>
+      UpateTransformationPage
+    </div>
+  )
+}
+
+export default UpdateTransformationPage
